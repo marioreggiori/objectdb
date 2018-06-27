0abea7f77952cfea2fcd4da0edec05599000b03a
marioIM <mail@mario.im>
2018-06-27 06:38:16 +0200

    added example

---
+++ example/main.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+import 'package:objectdb/objectdb.dart';
+
+void main() async {
+
+  // open db
+  var db = ObjectDB('/some/path/to/file.db');
+  await db.open();
+
+  // insert documents
+  var ids = await db.insertMany([
+    {"name": {"first": "Maria", "last": "Smith"}, "age": 20, "active": false},
+    {"name": {"first": "James", "last": "Jones"}, "age": 32, "active": false},
+  ]);
+
+  // update documents
+  db.update({"name.first": "Maria"}, {"active": true});
+
+  // remove documents
+  db.remove({
+    Op.inList: {
+      "name.last": ["Jones", "Miller", "Wilson"]
+    },
+    "active": false,
+  });
+
+  // find documents
+  print(await db.find({
+    Op.lte: {"age": 30}
+  }));
+
+  // close db
+  await db.close();
+}
